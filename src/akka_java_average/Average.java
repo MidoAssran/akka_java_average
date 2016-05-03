7976383286585f9e5c4424bc7dfac76863604e41
MidoAssran <midostuf@hotmail.com>
2016-05-03 18:46:51 -0400

    Initial commit - Working node average computation, and node pi computation. Average computation uses a linked list node implementation but is adaptable to any kind of network architecture

---
+++ src/akka_java_average/Average.java
@@ -0,0 +1,250 @@
+/**
+ * Author: Mido Assran
+ * Date: Tue, May 3, 2016
+ * Description:
+ * 		This creates an abstract network of actors where 
+ * 		actors work together to compute the average
+ * 		of their abstract-network IDs.
+*/
+
+
+package akka_java_average;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.Inbox;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
+import akka.routing.ActorRefRoutee;
+import akka.routing.RoundRobinRoutingLogic;
+import akka.routing.Router;
+import akka.routing.Routee;
+
+import java.util.concurrent.TimeUnit;
+
+import com.mcgill.mido.java.Pi.Worker;
+
+public class Average {
+	
+	/*
+	 * Everything starts at main!
+	 */
+	public static void main(String[] args) {
+		Average avg = new Average();
+		avg.calculate(101);
+	}
+	
+	public void calculate(final int nrOfWorkers) {
+		// Create an AKKA system
+		ActorSystem system = ActorSystem.create("AvgSystem");
+
+		// Create the result listener, which will print the result and subsequently shutdown the machine
+		final ActorRef reporter = system.actorOf(Props.create(Reporter.class), "reporter");
+
+		// Create the node average computation coordinator
+		ActorRef coordinator = system.actorOf(Props.create(Coordinator.class, nrOfWorkers, reporter), "coordinator");
+
+		// start the calculation
+		coordinator.tell(new StartAverage(), ActorRef.noSender());
+	}
+	
+	
+	
+	/*------------------------------Messages-----------------------------------*
+	 * StartAverage: 		Sent to coordinator node to start worker node computation
+	 * SetPeer:				Sent to a worker node to set it's overlay-network peer
+	 * Compute: 			Sent to a worker node to continue running computation
+	 * Result: 				Sent from the worker to the coordinator actor containing the results
+	 * 	   					of the workers' computation
+	 * NodeAverage:		 	Sent from the coordinator actor to the reporter actor containing the final
+	 *		    			average result and how long (time) the calculation took.
+	 */
+	static class StartAverage {}
+	static class SetPeer {
+		private final ActorRef peer;
+		
+		public SetPeer(ActorRef peer){
+			this.peer = peer;
+		}
+		
+		public ActorRef getPeer(){
+			return peer;
+		}
+	}
+	static class Compute {
+		private double runningAverage;
+		private int nodesHit;
+		
+		public Compute(double runningAverage, int nodesHit) {
+			this.runningAverage = runningAverage;
+			this.nodesHit = nodesHit;
+		}
+		
+		public double getRunningAverage(){
+			return runningAverage;
+		}
+		public int getNodesHit(){
+			return nodesHit;
+		}
+	}
+	static class Result {
+		private double value;
+		
+		public Result (double value) {
+			this.value = value;
+		}
+		
+		public double getValue() {
+			return value;
+		}
+	}
+	static class NodeAverage {
+		private final double nodeAverage;
+		private final Duration duration;
+
+		public NodeAverage(double nodeAverage, Duration duration) {
+			this.nodeAverage = nodeAverage;
+			this.duration = duration;
+		}
+
+		public double getNodeAverage() {
+			return nodeAverage;
+		}
+
+		public Duration getDuration() {
+			return duration;
+		}
+	}
+	/*---------------------------END Messages-----------------------------------*/
+
+	
+	
+	/*--------------------------------Actors-------------------------------------*
+	 * Worker: 			nodes in the network whose id averages we try to compute
+	 * Coordinator:		Root node that spawns workers & coordinates their id average computation
+	 */
+	public static class Worker extends UntypedActor {
+		private final int uniqueId;
+		private ActorRef peer;
+		private final ActorRef coordinator;
+		private boolean didComputeOnce = false;
+				
+		public Worker (int uniqueId, ActorRef coordinator){
+			this.coordinator = coordinator;
+			this.uniqueId = uniqueId;
+		}
+		
+		public void onReceive(Object message) {
+			if (message instanceof Compute) { // compute the running average and pass on to peer or if already contributed pass up to supervisor
+				Compute computation = (Compute) message;
+				if (!didComputeOnce){
+					try {
+						double newAverage = calculateAverageFor(computation.getRunningAverage(),computation.getNodesHit());
+						int newNodesHit = computation.getNodesHit() + 1;
+						peer.tell(new Compute(newAverage, newNodesHit), getSelf());
+					} catch (NullPointerException e) {
+						// peer was not set
+						System.out.println(e.getMessage());
+					}
+				} else {
+					coordinator.tell(new Result(computation.getRunningAverage()), getSelf());
+				}
+			} else if (message instanceof SetPeer) { // set our peer
+				SetPeer setRPC = (SetPeer) message;
+				boolean status = setPeer(setRPC.getPeer()); // true if succeeds, false if fails
+			} else {
+				unhandled(message);
+			}
+		}
+
+		
+		private boolean didSetPeer = false; // a custom implementation to make subsequentPeer "final"
+		private boolean setPeer(ActorRef peer){
+			if (!didSetPeer) {
+				this.peer = peer;
+				didSetPeer = true;
+			}
+			return !didSetPeer;
+		}
+		
+		private double calculateAverageFor(double runningAverage, int nodesHit){
+			didComputeOnce = true;
+			double weightOld = (double)nodesHit/(double)(nodesHit + 1);
+			double weightNew = (double)1/(double)(nodesHit + 1);
+			double newAverage = (runningAverage*weightOld) + (uniqueId*weightNew);
+			return newAverage;
+		}
+	}
+	
+	public static class Coordinator extends UntypedActor {
+		
+		private final ActorRef reporter;
+		private final Router workerRouter;
+		private final int nrOfWorkers;
+		private double nodeAverage = 0;
+		private final long start = System.currentTimeMillis();				
+		private List<ActorRef> workers;
+
+		public Coordinator (final int nrOfWorkers, ActorRef reporter) {
+			this.reporter = reporter;
+			this.nrOfWorkers = nrOfWorkers;
+			
+			// create the worker nodes
+			workers = new ArrayList<ActorRef>();
+			List<Routee> routees = new ArrayList<Routee>();
+		    for (int i = 0; i < nrOfWorkers; i++) {
+		    	ActorRef r = getContext().actorOf(Props.create(Worker.class, i, getSelf()));
+				getContext().watch(r);
+				workers.add(r);
+				routees.add(new ActorRefRoutee(r));
+			}
+			workerRouter = new Router(new RoundRobinRoutingLogic(), routees);
+		    // set peers of worker nodes
+		    for (int i = 0; i < nrOfWorkers; i++){
+		    	  ActorRef worker = workers.get(i);
+			      if (i != nrOfWorkers-1){
+			    	  ActorRef peer = workers.get(i+1);
+			    	  worker.tell(new SetPeer(peer), ActorRef.noSender());
+			      } else { // last worker in our array; set peer to be itself
+			    	  worker.tell(new SetPeer(worker), ActorRef.noSender());
+			      }
+		    }
+		    
+		}
+
+		public void onReceive(Object message) {
+			if (message instanceof StartAverage) {
+				ActorRef worker = workers.get(0);
+				worker.tell(new Compute(0,0), getSelf());
+			} else if (message instanceof Result) {
+				Result result = (Result) message;
+				nodeAverage += result.getValue();
+				Duration duration = Duration.create(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
+				reporter.tell(new NodeAverage(nodeAverage, duration), getSelf());
+				getContext().stop(getSelf());
+			} else {
+				unhandled(message);
+			}
+		}
+	}
+
+	public static class Reporter extends UntypedActor {
+		public void onReceive(Object message) {
+			if (message instanceof NodeAverage) {
+				NodeAverage nodeAverage = (NodeAverage) message;
+				System.out.println(String.format("\n\tNode average: \t\t%s\n\tCalculation time: \t%s", nodeAverage.getNodeAverage(), nodeAverage.getDuration()));
+				getContext().system().shutdown();
+			} else {
+				unhandled(message);
+			}
+		}
+	}
+
+}
+
---
+++ src/com/mcgill/mido/java/Pi.java
@@ -0,0 +1,207 @@
+/**
+ * Author: Mido Assran
+ * Date: Tue, May 3, 2016
+ * Description:
+ * 	Computes the value of pi based on a series algorithm.
+ * 	This is a basic starter Akka project followed from a tutorial
+ * 	available at http://doc.akka.io/docs/akka/2.0.1/intro/getting-started-first-java.html
+ * 
+*/
+
+
+
+package com.mcgill.mido.java;
+
+import java.util.List;
+import java.util.ArrayList;
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.Inbox;
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
+
+import akka.routing.ActorRefRoutee;
+import akka.routing.RoundRobinRoutingLogic;
+import akka.routing.Router;
+import akka.routing.Routee;
+
+import java.util.concurrent.TimeUnit;
+
+public class Pi {
+
+	public static void main(String[] args) {
+		Pi pi = new Pi();
+		pi.calculate(4,10000,10000);
+	}
+
+	/*---------Messages-------*
+	 * Calculate: 		Sent to master to start calculation
+	 *
+	 * Work: 		Sent from master to worker actors containing the work assignment
+	 *
+	 * Result: 		Sent from the worker to the master actor containing the results
+	 * 	   		of the worker's computation
+	 *
+	 * PiApproximation: 	Sent from the master actor to the listener actor containing the final
+	 *		    	pi result and how long (time) the calculation took.
+	*/
+	static class Calculate {
+	}
+
+	static class Work {
+		private final int start;
+		private final int nrOfElements;
+
+		public Work(int start, int nrOfElements) {
+			this.start = start;
+			this.nrOfElements = nrOfElements;
+		}
+
+		public int getStart() {
+			return start;
+		}
+
+		public int getNrOfElements() {
+			return nrOfElements;
+		}
+	}
+
+	static class Result {
+		private final double value;
+
+		public Result(double value) {
+			this.value = value;
+		}
+
+		public double getValue() {
+			return value;
+		}
+	}
+
+	static class PiApproximation {
+		private final double pi;
+		private final Duration duration;
+
+		public PiApproximation(double pi, Duration duration) {
+			this.pi = pi;
+			this.duration = duration;
+		}
+
+		public double getPi() {
+			return pi;
+		}
+
+		public Duration getDuration() {
+			return duration;
+		}
+
+	}
+
+
+
+
+	public static class Worker extends UntypedActor {
+
+		public void onReceive(Object message) {
+			if (message instanceof Work) {
+				Work work = (Work) message;
+				double result = calculatePiFor(work.getStart(), work.getNrOfElements());
+				getSender().tell(new Result(result), getSelf());
+			} else {
+				unhandled(message);
+			}
+		}
+
+		private double calculatePiFor(int start, int nrOfElements) {
+			double acc = 0.0;
+			for (int i = start * nrOfElements; i <= ((start+1) * nrOfElements -1); i++) {
+				acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1);
+			}
+
+			return acc;
+		}
+	}
+
+
+	public static class Master extends UntypedActor {
+
+
+		private final int nrOfMessages;
+		private final int nrOfElements;
+
+		private double pi;
+		private int nrOfResults;
+		private final long start = System.currentTimeMillis();
+
+		private final ActorRef listener;
+		// private final ActorRef workerRouter;
+
+		private final Router workerRouter;
+
+		public Master(final int nrOfWorkers, int nrOfMessages, int nrOfElements, ActorRef listener) {
+			this.nrOfMessages = nrOfMessages;
+			this.nrOfElements = nrOfElements;
+			this.listener = listener;
+
+			List<Routee> routees = new ArrayList<Routee>();
+		    for (int i = 0; i < nrOfWorkers; i++) {
+		      ActorRef r = getContext().actorOf(Props.create(Worker.class));
+		      getContext().watch(r);
+		      routees.add(new ActorRefRoutee(r));
+		    }
+		    workerRouter = new Router(new RoundRobinRoutingLogic(), routees);
+
+		}
+
+
+		public void onReceive(Object message) {
+			if (message instanceof Calculate) {
+				for (int start=0; start < nrOfMessages; start++){
+					workerRouter.route(new Work(start, nrOfElements), getSelf());
+				}
+			} else if (message instanceof Result) {
+				Result result = (Result) message;
+				pi += result.getValue();
+				nrOfResults += 1;
+				if (nrOfResults == nrOfMessages) {
+					// send the result to the listener
+					Duration duration = Duration.create(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
+					listener.tell(new PiApproximation(pi,duration), getSelf());
+					// Stops this actor and all its supervised children
+					getContext().stop(getSelf());
+				}
+			} else {
+				unhandled(message);
+			}
+		}
+	}
+
+	public static class Listener extends UntypedActor {
+		public void onReceive(Object message) {
+			if (message instanceof PiApproximation) {
+				PiApproximation approximation = (PiApproximation) message;
+				System.out.println(String.format("\n\tPi approximation: \t%s\n\tCalculation time: \t%s", approximation.getPi(), approximation.getDuration()));
+				getContext().system().shutdown();
+			} else {
+				unhandled(message);
+			}
+		}
+	}
+
+	public void calculate(final int nrOfWorkers, final int nrOfElements, final int nrOfMessages) {
+		// Create an AKKA system
+		ActorSystem system = ActorSystem.create("PiSystem");
+
+		// Create the result listener, which will print the result and subsequently shutdown the machine
+		final ActorRef listener = system.actorOf(Props.create(Listener.class), "listener");
+
+		// Create the master
+		ActorRef master = system.actorOf(Props.create(Master.class, nrOfWorkers, nrOfMessages, nrOfElements, listener), "master");
+
+		// start the calculation
+		master.tell(new Calculate(), ActorRef.noSender());
+	}
+
+}
